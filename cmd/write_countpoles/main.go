0979022bf2882fbd8de733e7ecc4d4735da11ab7
m1chiel <michiel.vereecke@student.hogent.be>
2024-03-06 13:36:29 +0100

    added bike count pole data -1 year

---
+++ cmd/write_countpoles/main.go
@@ -0,0 +1,140 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"flag"
+	"log/slog"
+	"os"
+	"path/filepath"
+	h "stage2024/pkg/helper"
+	"stage2024/pkg/protogen/poles"
+	"strconv"
+	"sync"
+	"time"
+
+	"github.com/twmb/franz-go/pkg/kgo"
+	"github.com/twmb/franz-go/pkg/sr"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+// Historical data, so data is from 2023
+
+// updates every 5 minutes
+const fetchdelay = time.Minute * 5
+
+type ApiData struct {
+	Code          string    `json:"code"`
+	Locatie       string    `json:"locatie"`
+	Datum         string    `json:"datum"`
+	Uur5minuten   string    `json:"uur5minuten"`
+	Ordening      time.Time `json:"ordening"`
+	Totaal        string    `json:"totaal"`
+	Tegenrichting string    `json:"tegenrichting"`
+	Hoofdrichting string    `json:"hoofdrichting"`
+}
+
+func toInt(s string) int {
+	i, err := strconv.Atoi(s)
+	h.MaybeDieErr(err)
+	return i
+}
+
+func main() {
+	slog.Default()
+
+	seed := flag.String("seedbroker", "localhost:19092", "brokers port to talk to")
+	registry := flag.String("registry", "localhost:18081", "schema registry port to talk to")
+	topic := flag.String("topic", "countpoles", "topic to produce to and consume from")
+
+	slog.Info("Starting kafka client", "seedbrokers", *seed)
+	cl, err := kgo.NewClient(
+		kgo.SeedBrokers(*seed),
+		kgo.AllowAutoTopicCreation(),
+	)
+	h.MaybeDieErr(err)
+	defer cl.Close()
+
+	slog.Info("Starting schema registry client", "host", *registry)
+	rcl, err := sr.NewClient(sr.URLs(*registry))
+	h.MaybeDieErr(err)
+
+	file, err := os.ReadFile(filepath.Join("./proto", poles.File_poles_pole_proto.Path()))
+	h.MaybeDieErr(err)
+
+	sub := *topic + "-value"
+	ss, err := rcl.CreateSchema(context.Background(), sub, sr.Schema{
+		Schema: string(file),
+		Type:   sr.TypeProtobuf,
+	})
+	h.MaybeDieErr(err)
+	slog.Info("created or reusing schema",
+		"subject", ss.Subject,
+		"version", ss.Version,
+		"id", ss.ID,
+	)
+
+	var serde sr.Serde
+	serde.Register(
+		ss.ID,
+		&poles.PoleData{},
+		sr.EncodeFn(func(a any) ([]byte, error) {
+			return proto.Marshal(a.(*poles.PoleData))
+		}),
+		sr.Index(0),
+		sr.DecodeFn(func(b []byte, a any) error {
+			return proto.Unmarshal(b, a.(*poles.PoleData))
+		}),
+	)
+
+	var wg sync.WaitGroup
+
+	slog.Info("Producing records")
+	for {
+		wg.Add(1)
+		go func() {
+			slog.Info("Sleeping", "time", fetchdelay)
+			time.Sleep(fetchdelay)
+			wg.Done()
+		}()
+
+		urls := h.GetPoleUrls()
+
+		for _, url := range urls {
+			allItems := h.Fetch[*poles.PoleData](url,
+				func(b []byte) *poles.PoleData {
+					var in ApiData
+					json.Unmarshal(b, &in)
+
+					out := poles.PoleData{}
+					out.Code = in.Code
+					out.Locatie = in.Locatie
+					out.Datum = in.Datum
+					out.Uur5Minuten = in.Uur5minuten
+					out.Ordening = timestamppb.New(in.Ordening)
+					out.Totaal = int32(toInt(in.Totaal))
+					out.Tegenrichting = int32(toInt(in.Tegenrichting))
+					out.Hoofdrichting = int32(toInt(in.Hoofdrichting))
+
+					return &out
+				},
+			)
+			ctx := context.Background()
+			for _, item := range allItems {
+				wg.Add(1)
+				itemByte, err := serde.Encode(item)
+				h.MaybeDie(err, "Encoding error")
+				record := &kgo.Record{Topic: *topic, Value: itemByte}
+				cl.Produce(ctx, record, func(_ *kgo.Record, err error) {
+					defer wg.Done()
+					h.MaybeDie(err, "Producing")
+				})
+			}
+			slog.Info("fetched data", "src", url)
+		}
+
+		wg.Wait()
+		slog.Info("uploaded all records")
+	}
+}
---
+++ pkg/helper/poledata.go
@@ -0,0 +1,86 @@
+package helper
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log/slog"
+	"net/http"
+	"strings"
+	"time"
+)
+
+const maxRequestCount = 100
+
+type fetchData struct {
+	TotalCount int               `json:"total_count"`
+	Results    []json.RawMessage `json:"results"`
+}
+
+func GetPoleUrls() []string {
+	urls := []string{}
+	for _, paal := range []string{"coupure-links", "zuidparklaan", "visserij", "isabellakaai", "groendreef", "bataviabrug"} {
+		// date needs to be in 2023, code will stop working in 2025
+		date := time.Now().AddDate(-1, 0, 0).Format("2006-01-02")
+		hour := time.Now().Round(5 * time.Minute).Format("15:04:05")
+
+		splitted := strings.Split(hour, ":")
+		var hourformatted string
+		for _, s := range splitted {
+			hourformatted += s + "%3A"
+		}
+		hourformatted = hourformatted[:len(hourformatted)-3]
+
+		urls = append(urls, fmt.Sprintf("https://data.stad.gent/api/explore/v2.1/catalog/datasets/fietstelpaal-%s-2023-gent/records?where=datum%20%3D%%20date%%27%s%%27%%20and%%20uur5minuten%%20%%3D%%20%%27%s%%27", paal, date, hourformatted))
+	}
+	return urls
+}
+
+func Fetch[T any](url string, f func([]byte) T) []T {
+	offset := 0
+	totalCount := 1
+
+	allItems := make([]T, 0)
+
+	for offset < totalCount {
+		data, err := request[T](url, offset)
+		for _, x := range data.Results {
+			allItems = append(allItems, f(x))
+		}
+		if err != nil {
+			slog.Error("Error fetching data:", "error", err)
+			break
+		}
+		totalCount = data.TotalCount
+		offset += len(data.Results)
+	}
+
+	slog.Info("Total expected items", "count", totalCount)
+	slog.Info("Total items fetched", "count", len(allItems))
+
+	return allItems
+}
+
+func request[T any](url string, offset int) (fetchData, error) {
+	slog.Info("Makking request", "offset", offset)
+	resp, err := http.Get(fmt.Sprintf("%s&limit=%d&offset=%d", url, maxRequestCount, offset))
+	var data fetchData
+	if err != nil {
+		return data, err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return data, err
+	}
+
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		return data, err
+	}
+
+	slog.Info("Request result", "item_count", len(data.Results))
+
+	return data, nil
+}
