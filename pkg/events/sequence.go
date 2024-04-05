86b05b6d62d2c7cb80e71c86c6a1482063f377d6
m1chiel <michiel.vereecke@student.hogent.be>
2024-04-05 09:32:08 +0200

    moved randsleep function to helper

--- pkg/events/sequence.go
+++ pkg/events/sequence.go
@@ -2,7 +2,6 @@ package events
 
 import (
 	"log/slog"
-	"math"
 	"math/rand"
 	"stage2024/pkg/database"
 	h "stage2024/pkg/helper"
@@ -10,16 +9,10 @@ import (
 	"stage2024/pkg/protogen/bikes"
 	"stage2024/pkg/protogen/common"
 	"stage2024/pkg/protogen/stations"
-	"time"
 
 	"github.com/brianvoe/gofakeit/v7"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
-
-const warpSpeed = true
-const chanceAbandond = 0.01
-const chanceDefect = 0.01
-const chanceImmobilized = 0.5
 
 func RunSequence(kc *kafka.Client) {
 	slog.Info("Starting sequence")
@@ -56,7 +49,7 @@ func RunSequence(kc *kafka.Client) {
 	})
 	h.MaybeDieErr(err)
 
-	randSleep(60*5, 60)
+	h.RandSleep(60*5, 60)
 
 	err = kc.Produce(&bikes.BikePickedUp{
 		TimeStamp: timestamppb.Now(),
@@ -73,9 +66,9 @@ func RunSequence(kc *kafka.Client) {
 	})
 	h.MaybeDieErr(err)
 
-	randSleep(60*12, 60)
+	h.RandSleep(60*12, 60)
 
-	if rand.Float32() <= chanceAbandond {
+	if rand.Float32() <= chanceAbandoned {
 		err = kc.Produce(&bikes.BikeAbandoned{
 			TimeStamp: timestamppb.Now(),
 			Bike: &bikes.AbandonedBike{
@@ -128,7 +121,7 @@ func RunSequence(kc *kafka.Client) {
 	dropoffStation := database.Station{}
 	db.Order("random()").Find(&dropoffStation)
 
-	randSleep(60*2, 60)
+	h.RandSleep(60*2, 60)
 
 	err = kc.Produce(&bikes.BikeReturned{
 		TimeStamp: timestamppb.Now(),
@@ -145,27 +138,4 @@ func RunSequence(kc *kafka.Client) {
 	})
 	h.MaybeDieErr(err)
 
-}
-
-func generateNormalDuration(mean, stdDev float64) float64 {
-	// Generate two random numbers from a uniform distribution
-	u1 := rand.Float64()
-	u2 := rand.Float64()
-
-	// Apply Box-Muller transform to get values from a standard normal distribution
-	z0 := math.Sqrt(-2*math.Log(u1)) * math.Cos(2*math.Pi*u2)
-
-	// Scale and shift the values to get values from a normal distribution with given mean and standard deviation
-	duration := mean + z0*stdDev
-
-	return duration
-}
-
-func randSleep(mean, stdDev int) {
-	t := time.Second*time.Duration(generateNormalDuration(float64(mean), float64(stdDev))) + time.Second*30
-	if warpSpeed {
-		t = t / 60
-	}
-	slog.Info("Sleeping", "time", t)
-	time.Sleep(t)
 }
---
+++ pkg/helper/sleep.go
@@ -0,0 +1,34 @@
+package helper
+
+import (
+	"log/slog"
+	"math"
+	"math/rand"
+	"time"
+)
+
+// warpSpeed is a boolean that determines whether the sleep durations are in real time or in warp speed
+var warpSpeed = true
+
+func GenerateNormalDuration(mean, stdDev float64) float64 {
+	// Generate two random numbers from a uniform distribution
+	u1 := rand.Float64()
+	u2 := rand.Float64()
+
+	// Apply Box-Muller transform to get values from a standard normal distribution
+	z0 := math.Sqrt(-2*math.Log(u1)) * math.Cos(2*math.Pi*u2)
+
+	// Scale and shift the values to get values from a normal distribution with given mean and standard deviation
+	duration := mean + z0*stdDev
+
+	return duration
+}
+
+func RandSleep(mean, stdDev int) {
+	t := time.Second*time.Duration(GenerateNormalDuration(float64(mean), float64(stdDev))) + time.Second*30
+	if warpSpeed {
+		t = t / 60
+	}
+	slog.Info("Sleeping", "time", t)
+	time.Sleep(t)
+}
