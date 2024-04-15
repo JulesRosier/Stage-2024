95b73fea48c2dae7f410117185dbc2d0a60dfde3
m1chiel <michiel.vereecke@student.hogent.be>
2024-04-15 10:31:25 +0200

    changed change detected to only take stations

--- pkg/database/change_detected.go
+++ pkg/database/change_detected.go
@@ -1,28 +1,13 @@
 package database
 
 import (
-	"log/slog"
 	"stage2024/pkg/helper"
 
 	"gorm.io/gorm"
 )
 
-// Reacts to change in database, sends appropiate events
-func ChangeDetected(change helper.Change, db *gorm.DB) error {
-	//set kafka client to global var
-
-	switch change.Table {
-	case "Bike":
-		slog.Warn("DEPRECATED, don't send events from db update")
-	case "Station":
-		err := stationchange(change, db)
-		return err
-	}
-	return nil
-}
-
 // Selects right events to send based on change for stations
-func stationchange(change helper.Change, db *gorm.DB) error {
+func ChangeDetected(change helper.Change, db *gorm.DB) error {
 	station, err := GetStationById(change.Id, db)
 	if err != nil {
 		return err
